ee5d8ae72440ba21646317c6ff1fd0f50aadcf03
jan <hudakjan83@gmail.com>
2015-11-29 20:14:37 +0100

    novy eshop k24

---
+++ src/test/java/sk/K24EshopProductParserTest.java
@@ -0,0 +1,26 @@
+package sk;
+
+import sk.hudak.pricecomparator.client.ProductInfoToString;
+import sk.hudak.pricecomparator.middle.api.canonical.ParserInputData;
+import sk.hudak.pricecomparator.middle.api.canonical.Unit;
+import sk.hudak.pricecomparator.middle.api.model.EshopProductInfo;
+import sk.hudak.pricecomparator.server.parser.K24EshopProductParser;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by jan on 29. 11. 2015.
+ */
+public class K24EshopProductParserTest {
+    public static void main(String[] args) {
+        ParserInputData parserInputData = new ParserInputData(
+                80,
+                Unit.KUS,
+                new BigDecimal(1),
+                "http://www.k24.sk/product/288078/Elektronika/Sport_a_hobby/Pampers_Premium_Care_VP_Mini_80_ks_.html");
+
+        K24EshopProductParser parser = new K24EshopProductParser();
+        EshopProductInfo productInfo = parser.getProductInfo(parserInputData);
+        System.out.println(ProductInfoToString.toString(productInfo));
+    }
+}
