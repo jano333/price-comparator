f5942207dceba7df82a6b291d95a7d72768cc69e
jan <hudakjan83@gmail.com>
2015-12-04 19:05:38 +0100

    novy eshop - perinbaba

---
+++ src/main/java/sk/hudak/pricecomparator/server/parser/PerinbabaEshopProductParser.java
@@ -0,0 +1,32 @@
+package sk.hudak.pricecomparator.server.parser;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import sk.hudak.pricecomparator.middle.api.model.EshopProductInfo;
+import sk.hudak.pricecomparator.server.core.AbstractEshopProductInfo;
+import sk.hudak.pricecomparator.server.core.AbstractEshopProductParser;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by jan on 4. 12. 2015.
+ */
+public class PerinbabaEshopProductParser extends AbstractEshopProductParser {
+    @Override
+    protected EshopProductInfo parsePrice(Document document) {
+        //cena
+        Elements elements = document.select("span[class=price]");
+        StringBuffer sb = new StringBuffer(elements.get(0).text());
+        sb = sb.deleteCharAt(sb.length() - 1);
+        sb = sb.deleteCharAt(sb.length() - 1);
+        final String cenaZaBalenie = sb.toString().replace(",", ".");
+
+        return new AbstractEshopProductInfo(parserInputData) {
+
+            @Override
+            public BigDecimal getPriceForPackage() {
+                return new BigDecimal(cenaZaBalenie);
+            }
+        };
+    }
+}
