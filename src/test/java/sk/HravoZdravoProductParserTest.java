158f3759fa63d22b99b7be040a0fbec836d26353
jan <hudakjan83@gmail.com>
2016-01-01 22:55:09 +0100

    nove eshop www.hravo-zdravo.sk

---
+++ src/test/java/sk/HravoZdravoProductParserTest.java
@@ -0,0 +1,27 @@
+package sk;
+
+import sk.hudak.pricecomparator.client.ProductInfoToString;
+import sk.hudak.pricecomparator.middle.api.canonical.ParserInputData;
+import sk.hudak.pricecomparator.middle.api.canonical.Unit;
+import sk.hudak.pricecomparator.middle.api.model.EshopProductInfo;
+import sk.hudak.pricecomparator.server.parser.HravoZdravoProductParser;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by jan on 1. 1. 2016.
+ */
+public class HravoZdravoProductParserTest {
+
+    public static void main(String[] args) {
+        ParserInputData parserInputData = new ParserInputData(
+                6,
+                Unit.KUS,
+                new BigDecimal(1),
+                "https://www.hravo-zdravo.sk/produkt/nutrilon-1-6x800g");
+
+        HravoZdravoProductParser parser = new HravoZdravoProductParser();
+        EshopProductInfo productInfo = parser.getProductInfo(parserInputData);
+        System.out.println(ProductInfoToString.toString(productInfo));
+    }
+}
