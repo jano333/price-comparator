bd86a421bb26e7be62dd7273eac5576b94c81f8f
jan <hudakjan83@gmail.com>
2016-01-04 19:52:58 +0100

    DR max eshop parser

---
+++ src/main/java/sk/hudak/pricecomparator/server/parser/DrMaxEshopProductParser.java
@@ -0,0 +1,32 @@
+package sk.hudak.pricecomparator.server.parser;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import sk.hudak.pricecomparator.middle.api.model.EshopProductInfo;
+import sk.hudak.pricecomparator.server.core.AbstractEshopProductInfo;
+import sk.hudak.pricecomparator.server.core.AbstractEshopProductParser;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by jan on 3. 1. 2016.
+ */
+public class DrMaxEshopProductParser extends AbstractEshopProductParser {
+
+    @Override
+    protected EshopProductInfo parsePrice(Document document) {
+
+        Elements elements = document.select("strong[itemprop=price]");
+        StringBuffer sb = new StringBuffer(elements.get(0).text());
+        final String cenaZaBalenie = sb.toString().replace(",", ".");
+
+        return new AbstractEshopProductInfo(parserInputData) {
+
+            @Override
+            public BigDecimal getPriceForPackage() {
+                return new BigDecimal(cenaZaBalenie);
+            }
+        };
+
+    }
+}
