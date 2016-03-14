0160e230c4b19d06203bbee57ed25fbd98c2ef4c
jan <hudakjan83@gmail.com>
2016-03-14 22:04:29 +0100

    wicket draft

---
+++ src/main/java/sk/hudak/pricecomparator/client/wicket/page/common/MyLayoutPage.java
@@ -0,0 +1,17 @@
+package sk.hudak.pricecomparator.client.wicket.page.common;
+
+/**
+ * Created by jan on 12. 3. 2016.
+ */
+public class MyLayoutPage extends MyRootPage {
+
+    public MyLayoutPage() {
+    }
+
+    //TODO overit co toto je za nastavenie
+    @Override
+    public boolean isVersioned() {
+        return false;
+    }
+
+}
---
+++ src/main/java/sk/hudak/pricecomparator/client/wicket/page/common/MyRootPage.java
@@ -0,0 +1,32 @@
+package sk.hudak.pricecomparator.client.wicket.page.common;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.http.WebResponse;
+
+/**
+ * Created by jan on 12. 3. 2016.
+ */
+public class MyRootPage extends WebPage {
+
+    public MyRootPage() {
+        add(new Label("pagetitle", getTitleModel()));
+    }
+
+    @Override
+    protected void setHeaders(WebResponse response) {
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-store, no-cache, max-age=0, must-revalidate");
+    }
+
+    protected IModel<String> getTitleModel() {
+        return new Model<>(getTitle());
+    }
+
+    protected String getTitle() {
+        return "TODO";
+    }
+
+}
