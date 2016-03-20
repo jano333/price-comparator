47534e5a3732aaeea2146ef7337322ff537ba425
jan <hudakjan83@gmail.com>
2016-03-20 11:29:06 +0100

    Zoznam skupin page + refaktor

---
+++ src/main/java/sk/hudak/pricecomparator/client/wicket/page/group/GroupProductPriceListPage.java
@@ -0,0 +1,20 @@
+package sk.hudak.pricecomparator.client.wicket.page.group;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
+import sk.hudak.pricecomparator.client.wicket.page.common.LayoutPage;
+
+/**
+ * Created by jan on 18. 3. 2016.
+ */
+public class GroupProductPriceListPage extends LayoutPage {
+
+    public GroupProductPriceListPage() {
+        add(new GroupProductPriceListTable("table"));
+    }
+
+    @Override
+    protected IModel<String> getTitleModel() {
+        return new ResourceModel("GroupProductPriceListPage.title");
+    }
+}
