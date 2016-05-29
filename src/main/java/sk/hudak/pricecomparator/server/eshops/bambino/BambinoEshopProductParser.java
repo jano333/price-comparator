07993e30cfaca4ff4ea8248f6a39b61a9993bbfb
jan <hudakjan83@gmail.com>
2016-05-29 20:48:20 +0200

    refaktor

--- src/main/java/sk/hudak/pricecomparator/server/eshops/bambino/BambinoEshopProductParser.java
+++ src/main/java/sk/hudak/pricecomparator/server/eshops/bambino/BambinoEshopProductParser.java
@@ -16,6 +16,11 @@ public class BambinoEshopProductParser extends AbstractEshopProductParser {
 
     @Override
     protected EshopProductInfo parsePrice(Document document) {
+//        if (isProductNedostupny(document)) {
+//            logger.error("produkt nie je dostupny: " + parserInputData.getEshopProductPage());
+//            return ProductInfoFactory.createUnaviable();
+//        }
+
         //cena
         Elements elements = document.select("strong[itemprop=price]");
         StringBuffer sb = new StringBuffer(elements.get(0).text());
@@ -30,5 +35,9 @@ public class BambinoEshopProductParser extends AbstractEshopProductParser {
                 return new BigDecimal(cenaZaBalenie);
             }
         };
+    }
+
+    private boolean isProductNedostupny(Document document) {
+        return notExistElement(document, "button[class=add_to_cart]");
     }
 }
