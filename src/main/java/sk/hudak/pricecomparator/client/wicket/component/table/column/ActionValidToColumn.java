352caa07d8e2c1d1fb1da2c74af0a28d49ec888e
jan <hudakjan83@gmail.com>
2016-07-03 17:06:59 +0200

    action column

---
+++ src/main/java/sk/hudak/pricecomparator/client/wicket/component/table/column/ActionValidToColumn.java
@@ -0,0 +1,33 @@
+package sk.hudak.pricecomparator.client.wicket.component.table.column;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.IConverter;
+import org.apache.wicket.util.convert.converter.DateConverter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by jan on 17. 6. 2016.
+ */
+public class ActionValidToColumn extends Label {
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+
+    public ActionValidToColumn(String id, IModel<Date> model) {
+        super(id, model);
+    }
+
+    @Override
+    public <C> IConverter<C> getConverter(Class<C> type) {
+        return (IConverter<C>) new DateConverter() {
+            @Override
+            public DateFormat getDateFormat(Locale locale) {
+                return sdf;
+            }
+        };
+    }
+}
