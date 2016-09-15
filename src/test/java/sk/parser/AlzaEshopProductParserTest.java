86da69e70fff45653f3021e3ae1f8495f4cb5009
jan <hudakjan83@gmail.com>
2016-09-15 14:54:52 +0200

    refaktor

--- src/test/java/sk/parser/AlzaEshopProductParserTest.java
+++ src/test/java/sk/parser/AlzaEshopProductParserTest.java
@@ -1,26 +1,25 @@
 package sk.parser;
 
 import sk.hudak.pricecomparator.middle.canonical.Unit;
-import sk.hudak.pricecomparator.server.tobedeleted.AlzaEshopProductParser;
-import sk.hudak.pricecomparator.server.tobedeleted.EshopProductInfo;
-import sk.hudak.pricecomparator.server.tobedeleted.ParserInputData;
+import sk.hudak.pricecomparator.server.async.ng.EshopParserRequestNg;
+import sk.hudak.pricecomparator.server.async.ng.EshopParserResponseNg;
+import sk.hudak.pricecomparator.server.eshops.alza.AlzaProductParser;
 
 import java.math.BigDecimal;
 
 /**
  * Created by jan on 12. 11. 2015.
  */
-@Deprecated
 public class AlzaEshopProductParserTest {
     public static void main(String[] args) {
-        ParserInputData parserInputData = new ParserInputData(
-                80,
-                Unit.DAVKA,
-                new BigDecimal(1),
-                "https://www.alza.sk/maxi/ariel-mountain-spring-6-kg-80-pranie-d2643269.htm");
+        EshopParserRequestNg parserInputData = new EshopParserRequestNg()
+                .setCountOfItemInOnePackage(80)
+                .setUnit(Unit.DAVKA)
+                .setCountOfUnit(new BigDecimal(1))
+                .setEshopProductPage("https://www.alza.sk/maxi/ariel-mountain-spring-6-kg-80-pranie-d2643269.htm");
 
-        AlzaEshopProductParser parser = new AlzaEshopProductParser();
-        EshopProductInfo productInfo = parser.getProductInfo(parserInputData);
-        System.out.println("");
+        AlzaProductParser parser = new AlzaProductParser();
+        EshopParserResponseNg productInfo = parser.parseEshopProductInfo(parserInputData);
+        System.out.println(productInfo);
     }
 }
--- src/test/java/sk/parser/K24EshopProductParserTest.java
+++ src/test/java/sk/parser/K24EshopProductParserTest.java
@@ -1,27 +1,25 @@
 package sk.parser;
 
-import sk.hudak.pricecomparator.client.ProductInfoToString;
 import sk.hudak.pricecomparator.middle.canonical.Unit;
-import sk.hudak.pricecomparator.server.tobedeleted.EshopProductInfo;
-import sk.hudak.pricecomparator.server.tobedeleted.K24EshopProductParser;
-import sk.hudak.pricecomparator.server.tobedeleted.ParserInputData;
+import sk.hudak.pricecomparator.server.async.ng.EshopParserRequestNg;
+import sk.hudak.pricecomparator.server.async.ng.EshopParserResponseNg;
+import sk.hudak.pricecomparator.server.eshops.k24.K24ProductParser;
 
 import java.math.BigDecimal;
 
 /**
  * Created by jan on 29. 11. 2015.
  */
-@Deprecated
 public class K24EshopProductParserTest {
     public static void main(String[] args) {
-        ParserInputData parserInputData = new ParserInputData(
-                80,
-                Unit.KUS,
-                new BigDecimal(1),
-                "http://www.k24.sk/product/288078/Elektronika/Sport_a_hobby/Pampers_Premium_Care_VP_Mini_80_ks_.html");
+        EshopParserRequestNg parserInputData = new EshopParserRequestNg()
+                .setCountOfItemInOnePackage(80)
+                .setUnit(Unit.KUS)
+                .setCountOfUnit(new BigDecimal(1))
+                .setEshopProductPage("http://www.k24.sk/product/288078/Elektronika/Sport_a_hobby/Pampers_Premium_Care_VP_Mini_80_ks_.html");
 
-        K24EshopProductParser parser = new K24EshopProductParser();
-        EshopProductInfo productInfo = parser.getProductInfo(parserInputData);
-        System.out.println(ProductInfoToString.toString(productInfo));
+        K24ProductParser parser = new K24ProductParser();
+        EshopParserResponseNg productInfo = parser.parseEshopProductInfo(parserInputData);
+        System.out.println(productInfo);
     }
 }
